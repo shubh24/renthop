04f8564ac948dddc764188f2acce42af78c8506e
shubh24 <shubhankar24@gmail.com>
2017-02-15 00:36:43 +0530

    trying feature engg

--- explore.R
+++ explore.R
@@ -19,12 +19,12 @@ rf_h2o = function(t1, t2){
   write.table(t2, gzfile('./t2.csv.gz'),quote=F,sep=',',row.names=F)
   
   feature_names = names(t1)
-  feature_names = feature_names[! feature_names %in% c("created", "listing_id", "interest_level")]
+  feature_names = feature_names[! feature_names %in% c("created", "listing_id", "interest_level", "latitude", "longitude", "hour")]
   
   train_h2o = h2o.uploadFile("./t1.csv.gz", destination_frame = "train")
   test_h2o = h2o.uploadFile("./t2.csv.gz", destination_frame = "test")
-  rf = h2o.randomForest(x = feature_names, y = "interest_level", training_frame = train_h2o, ntree = 50)
-  print(h2o.varimp(rf))
+  rf = h2o.randomForest(x = feature_names, y = "interest_level", training_frame = train_h2o, ntree = 50) 
+  print(as.data.frame(h2o.varimp(rf))$variable)
   res = as.data.frame(predict(rf, test_h2o))
   
   return(res)
@@ -35,7 +35,7 @@ t1 <- data.table(bathrooms=unlist(df$bathrooms)
                  ,building_id=as.factor(unlist(df$building_id))
                  ,created=as.POSIXct(unlist(df$created))
                  ,n_photos = as.numeric(sapply(df$photos, length))
-                 ,n_description = as.numeric(sapply(df$description, nchar))
+                 ,n_description = log(as.numeric(sapply(df$description, nchar)))
                  # ,description=unlist(df$description) # parse errors
                  # ,display_address=unlist(df$display_address) # parse errors
                  ,latitude=unlist(df$latitude)
@@ -45,21 +45,25 @@ t1 <- data.table(bathrooms=unlist(df$bathrooms)
                                           df$latitude)
                  ,listing_id=unlist(df$listing_id)
                  ,manager_id=as.factor(unlist(df$manager_id))
-                 ,price=unlist(df$price)
+                 ,price=unlist(df$price),
+                 days_since = as.numeric(difftime(Sys.Date(), unlist(df$created)))
                  ,interest_level=as.factor(unlist(df$interest_level))
-                 # ,street_adress=unlist(df$street_address) # parse errors
+                 ,street_adress=as.factor(unlist(df$street_address)) # parse errors
 )
 
 t1[,":="(yday=yday(created)
-         ,month=month(created)
+         ,month=lubridate::month(created)
          ,mday=mday(created)
          ,wday=wday(created)
-         ,hour=hour(created))]
+         ,hour=lubridate::hour(created))]
 
 frq_features = table(unlist(df$features))
 top_features = names(frq_features[frq_features>1000]) 
 
-t1 = cbind(t1, t(sapply(df$features, function(x){as.factor(top_features %in% x)})))
+t1 = cbind(t1, t(sapply(df$features, function(x){as.numeric(top_features %in% x)})))
+for (i in c(1:25)){
+  t1[[paste0("V", i)]] = as.factor(t1[[paste0("V", i)]])
+}
 t1$bathrooms = as.factor(t1$bathrooms)
 t1$bedrooms = as.factor(t1$bedrooms)
 
@@ -70,6 +74,7 @@ t1_train <- t1[sample, ]
 t1_test <- t1[-sample, ]
 
 res_val = rf_h2o(t1_train, t1_test)
+confusionMatrix(table(res_val$predict, t1_test$interest_level))
 
 test = fromJSON("test.json")
 
@@ -94,10 +99,10 @@ t2 <- data.table(bathrooms=unlist(test$bathrooms)
 )
 
 t2[,":="(yday=yday(created)
-         ,month=month(created)
+         ,month=lubridate::month(created)
          ,mday=mday(created)
          ,wday=wday(created)
-         ,hour=hour(created))]
+         ,hour=lubridate::hour(created))]
 
 t2 = cbind(t2, t(sapply(df$features, function(x){as.numeric(top_features %in% x)})))
 t2$bathrooms = as.factor(t2$bathrooms)
