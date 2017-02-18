1cbaddf9bc10b97352ae08358e86e73e050e7b3a
shubh24 <shubhankar24@gmail.com>
2017-02-18 12:22:39 +0530

    count vec working, exported as csv

--- nlp_features.py
+++ nlp_features.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+import sklearn
 
 train_file =  "train.json"
 test_file = "test.json"
@@ -9,12 +10,34 @@ test_df = pd.read_json(test_file)
 train_text = train_df["description"].tolist()
 train_text += test_df["description"].tolist()
 
-tfidf = TfidfVectorizer(stop_words=u'english',ngram_range=(1,2),lowercase=True,max_df = 0.80, min_df = 0.1, use_idf = True, max_features = 100)
+from nltk.corpus import stopwords
+stop = list(stopwords.words('english'))
+stop.append("kagglemanager")
+stop.append("renthop")
+stop.append("com")
+stop.append("br")
+stop.append("email")
+stop.append("closets")
+stop.append("floors")
+stop.append("york")
+
+
+tfidf = TfidfVectorizer(stop_words=stop,ngram_range=(1,2),lowercase=True,max_df = 0.70, min_df = 0.1, use_idf = True, max_features = 100)
 
 tfs = tfidf.fit_transform(train_text)
 feature_names = tfidf.get_feature_names()
 
-import cPickle as pickle
-pickle.dump(feature_names, open('feature_names.pkl', 'wb'))
+# import cPickle as pickle
+# pickle.dump(feature_names, open('feature_names.pkl', 'wb'))
 
-print feature_names
+cv = sklearn.feature_extraction.text.CountVectorizer(vocabulary=feature_names)
+res = cv.fit_transform(train_text).toarray()
+
+for i in range(0, 100):
+	print i, feature_names[i]
+# print res
+
+import csv
+with open("count_vec.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(res)
