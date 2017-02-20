e881fc7eafb6879013c9185b799f2265025c87ab
shubh24 <shubhankar24@gmail.com>
2017-02-21 00:13:28 +0530

    abstracted in function

--- rev_geocode.py
+++ rev_geocode.py
@@ -1,58 +1,41 @@
-import json
-import urllib2
 import pandas as pd
 import csv
-
 import reverse_geocoder as rg
-
-# def get_geonames(lat, lng, types):
-#     url = 'http://maps.googleapis.com/maps/api/geocode/json' + \
-#             '?latlng={},{}&sensor=false'.format(lat, lng)
-#     jsondata = json.load(urllib2.urlopen(url))
-
-#     address_comps = jsondata['results'][0]['address_components']
-
-#     filter_method = lambda x: len(set(x['types']).intersection(types))
-#     return filter(filter_method, address_comps)
-
-# types = ['neighborhood']
 
 train_file =  "train.json"
 test_file = "test.json"
 train_df = pd.read_json(train_file)
 test_df = pd.read_json(test_file)
 
-train_lat = train_df[["listing_id", "latitude", "longitude"]]
-test_lat = test_df[["listing_id", "latitude", "longitude"]]
+train_coords = train_df[["listing_id", "latitude", "longitude"]]
+test_coords = test_df[["listing_id", "latitude", "longitude"]]
 
-lat_lon = []
-listings = []
-for i, j in train_lat.iterrows():
-	lat_lon.append((j["latitude"], j["longitude"]))
-	listings.append(j["listing_id"])
+def go_do(train_var):
+    
+    lat_lon = []
+    listings = []
 
-results = rg.search(lat_lon)
-nbd_train = [[listings[i], results[i]['name']] for i in range(0, len(listings))]
+    if train_var == 1:
+        df = train_coords
+        out_file = "neighborhood_train.csv"        
+    else:
+        df = test_coords
+        out_file = "neighborhood_test.csv"
 
-with open("neighborhood_train.csv", "wb") as f:
+    for i, j in df.iterrows():
+    	lat_lon.append((j["latitude"], j["longitude"]))
+    	listings.append(int(j["listing_id"]))
 
-    writer = csv.writer(f, delimiter = ",")
+    results = rg.search(lat_lon)
+    nbd = [[listings[i], results[i]['name']] for i in range(0, len(listings))]
 
-    writer.writerow(["listing_id", "neighborhood"])
-    writer.writerows(nbd_train)
+    with open(out_file, "wb") as f:
 
-lat_lon = []
-listings = []
-for i, j in test_lat.iterrows():
-	lat_lon.append((j["latitude"], j["longitude"]))
-	listings.append(j["listing_id"])
+        writer = csv.writer(f, delimiter = ",")
 
-results = rg.search(lat_lon)
-nbd_test = [[listings[i], results[i]['name']] for i in range(0, len(listings))]
+        writer.writerow(["listing_id", "neighborhood"])
+        writer.writerows(nbd)
 
-with open("neighborhood_test.csv", "wb") as f:
-
-    writer = csv.writer(f, delimiter = ",")
-
-    writer.writerow(["listing_id", "neighborhood"])
-    writer.writerows(nbd_test)
+if __name__ == '__main__':
+    go_do(0)
+    go_do(1)
