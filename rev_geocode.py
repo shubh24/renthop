2c2947bdea95a702e042cafa082135952ba3a7da
shubh24 <shubhankar24@gmail.com>
2017-02-27 14:42:27 +0530

    neighborhoods hardcoded, min dist taken

--- rev_geocode.py
+++ rev_geocode.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 import reverse_geocoder as rg
+from geopy.distance import great_circle
 
 train_file =  "train.json"
 test_file = "test.json"
@@ -9,6 +10,12 @@ test_df = pd.read_json(test_file)
 
 train_coords = train_df[["listing_id", "latitude", "longitude"]]
 test_coords = test_df[["listing_id", "latitude", "longitude"]]
+
+coded_locs = {}
+with open("coded_locs.csv", "r") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        coded_locs[row[0]] = (row[1], row[2])
 
 def go_do(train_var):
     
@@ -23,8 +30,8 @@ def go_do(train_var):
         out_file = "neighborhood_test.csv"
 
     for i, j in df.iterrows():
-    	lat_lon.append((j["latitude"], j["longitude"]))
-    	listings.append(int(j["listing_id"]))
+        lat_lon.append((j["latitude"], j["longitude"]))
+        listings.append(int(j["listing_id"]))
 
     results = rg.search(lat_lon)
     nbd = [[listings[i], results[i]['name']] for i in range(0, len(listings))]
@@ -36,6 +43,39 @@ def go_do(train_var):
         writer.writerow(["listing_id", "neighborhood"])
         writer.writerows(nbd)
 
+def coded_locs_dist(train_var):
+
+    results = []
+
+    if train_var == 1:
+        df = train_coords
+        out_file = "neighborhood_train.csv"        
+    else:
+        df = test_coords
+        out_file = "neighborhood_test.csv"
+
+    for i, j in df.iterrows():
+        lat_lon = (j["latitude"], j["longitude"])
+
+        min_dist = 32768
+
+        for loc in coded_locs:
+            dist = great_circle(lat_lon, coded_locs[loc]).km
+
+            if dist < min_dist:
+                min_dist = dist
+                closest_loc = loc
+
+        results.append([int(j["listing_id"]), closest_loc])
+
+    with open(out_file, "wb") as f:
+        writer = csv.writer(f, delimiter = ",")
+        writer.writerow(["listing_id", "neighborhood"])
+        writer.writerows(results)
+
+
 if __name__ == '__main__':
-    go_do(0)
-    go_do(1)
+    # go_do(0)
+    # go_do(1)
+    # coded_locs_dist(0)
+    coded_locs_dist(1)    
