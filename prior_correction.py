35fe240e8986af6e93b1cb3dea83c997e87dc630
shubh24 <shubhankar24@gmail.com>
2017-04-05 14:26:57 +0530

    normalize classes

---
+++ prior_correction.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+file = str(raw_input())
+df = pd.read_csv(file+".csv")
+
+interest_levels = ['low', 'medium', 'high']
+
+tau = {
+    'low': 0.69195995, 
+    'medium': 0.23108864,
+    'high': 0.07695141, 
+}
+
+def correct(df):
+    y = df[interest_levels].mean()
+    a = [tau[k] / y[k]  for k in interest_levels]
+    print a
+
+    def f(p):
+        for k in range(len(interest_levels)):
+            p[k] *= a[k]
+        return p / p.sum()
+
+    df_correct = df.copy()
+    df_correct[interest_levels] = df_correct[interest_levels].apply(f, axis=1)
+
+    y = df_correct[interest_levels].mean()
+    a = [tau[k] / y[k]  for k in interest_levels]
+    print a
+
+    return df_correct
+
+df_correct = correct(df)
+
+df_correct.to_csv(file + '_corrected.csv')
